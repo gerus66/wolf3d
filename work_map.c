92135bc2ec86a26fffa99ea2fc739353599bcfa6
Merlon crakehall Bartoletti <mbartole@e3r3p9.21-school.ru>
2019-03-23 00:15:12 +0300

    it almost works in range 0 - pi grad

--- work_map.c
+++ work_map.c
@@ -6,108 +6,122 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/20 20:42:36 by mbartole          #+#    #+#             */
-/*   Updated: 2019/03/22 18:58:13 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/03/23 00:03:10 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
-/*
-void	work_map(t_sdl *sbox, unsigned int *map, int len)
-{
-	int		i;
-	int		map_size;
-	t_point	*cp;
 
-	i = -1;
-	map_size = sbox->map->len / sizeof(t_point);
-	cp = (t_point *)sbox->map->cont;
-	printf("%f, %f\n", STEP, DIST);
-	while (++i < map_size)
-	{
-		map[i] = 0x00FF;
-//		if (cp[i].h)
-//	    	map[100500 + i % sbox->map->offset + i / sbox->map->offset * len] = 0x42F4AD;
-//		else
-//			map[100500 + i % sbox->map->offset + i / sbox->map->offset * len] = 0xFFFFFF;
-	}
-}*/
-
-int	get_dist(t_sdl *sbox, float ang, int len)
+static float	get_dist_x(t_sdl *sbox, float ang)
 {
 	t_point *cp;
+	int	x;
+	int	cur_r;
+	int	cur_c;
+	int xa;
+	float dist = 0.0;
+
+	cur_r = sbox->cam.y / BLOCK;
+	xa = ABS(BLOCK / tan(ang));
+	x = sbox->cam.x + (sbox->cam.y - cur_r * BLOCK) / tan(ang);
+	cur_c = x / BLOCK;
 	cp = (t_point *)sbox->map->cont;
-
-	int	x = sbox->cam.x;
-	int	y = sbox->cam.y;
-	int	cur_r = y / BLOCK - 1;
-	int	cur_c = x / BLOCK - 1;
-
-	float xa = BLOCK / tan(ang);
-	float ya = BLOCK * tan(ang);
-	int dist = 0;
-	printf("X: %.1f %d\n", xa, signbit(xa));
-	while (cur_c >= 0 && cur_r >= 0 && cur_c < len)
+//	printf("|angle: %.2f x_st: %d xa: %d row: %d col: %d|",
+//			ang, x, xa, cur_r, cur_c);
+	while (cur_c >= 0 && cur_r >= 0 && cur_c < WIN_W && cur_r < WIN_H)
 	{
-		x -= xa;
-		cur_c = x / BLOCK - 1;
 		if (cp[sbox->map->offset * cur_r + cur_c].h)
 		{
+			printf("x[%d, %d] ", cur_c, cur_r);
 			dist = sqrt(pow((sbox->cam.y / BLOCK - cur_r) * BLOCK + 
 					(int)(0.5 * BLOCK), 2) + pow(x - sbox->cam.x, 2));
-			break ;
+			return (dist);
 		}
-		cur_r--;
+		cur_r = (ang > 0) ? cur_r - 1 : cur_r + 1;
+		x = (ang > -M_PI / 2 && ang < M_PI / 2) ? x + xa : x - xa;
+		cur_c = x / BLOCK;
 	}
-	if (dist)
-		return (dist);
-	x = sbox->cam.x;
-	y = sbox->cam.y;
-	cur_r = y / BLOCK - 1;
-	cur_c = x / BLOCK - 1;
-	printf("Y: \n");
-	while (cur_c >= 0  && cur_r >= 0 && cur_c < len)
+	return (0.0);
+}
+
+static float	get_dist_y(t_sdl *sbox, float ang)
+{
+	t_point *cp;
+	int	y;
+	int	cur_r;
+	int	cur_c;
+	int ya;
+	float dist = 0.0;
+
+	cur_c = sbox->cam.x / BLOCK;
+	ya = ABS(BLOCK * tan(ang));
+	y = (ang < M_PI / 2 && ang > -M_PI / 2) ? (cur_c + 1) * BLOCK - sbox->cam.x :
+		cur_c * BLOCK - sbox->cam.x;
+	y = sbox->cam.y - y * tan(ang);
+	cur_r = y / BLOCK;
+	cp = (t_point *)sbox->map->cont;
+//	printf("|angle: %.2f y_st: %d ya: %d row: %d col: %d|",
+//			ang, y, ya, cur_r, cur_c);
+	while (cur_c >= 0 && cur_r >= 0 && cur_c < WIN_W && cur_r < WIN_H)
 	{
-		y -= ya;
-		cur_r = y / BLOCK - 1;
 		if (cp[sbox->map->offset * cur_r + cur_c].h)
 		{
+		printf("y[%d, %d] ", cur_c, cur_r);
 			dist = sqrt(pow((sbox->cam.x / BLOCK - cur_c) * BLOCK + 
 					(int)(0.5 * BLOCK), 2) + pow(y - sbox->cam.y, 2));
-			break ;
+			return (dist);
 		}
-		cur_c++;
+		cur_c = (ang < M_PI / 2 && ang > -M_PI / 2) ? cur_c + 1 : cur_c - 1;
+		y = (ang > 0) ? y - ya : y + ya;
+		cur_r = y / BLOCK;
 	}
-	return (dist);	
+	return (0.0);
 }
 
 void	work_it(t_sdl *sbox, unsigned int *map, int len)
 {
-	int	i = -1;
-	int	j = -1;
+	int	i;
+	int	j;
 	float dist;
+	float ydist;
 	int h;
-	float ang = sbox->cam.angle - FOV / 2;
+	float ang;
 
-	dist = get_dist(sbox, sbox->cam.angle, len);
-	printf("angle: %.0f\n", sbox->cam.angle / M_PI * 180);
-	dist *= cos(ang - sbox->cam.angle);
-	printf("dist %f, main height %f\n", dist, (WALL_H * DIST / dist));
+	len++;//
+	if (sbox->cam.angle > M_PI)
+		sbox->cam.angle = -(2 * M_PI - sbox->cam.angle);
+	if (sbox->cam.angle < -M_PI)
+		sbox->cam.angle = 2 * M_PI + sbox->cam.angle;
+	printf("from %d to %d grad\n", (int)((sbox->cam.angle - FOV / 2) / M_PI * 180),
+			(int)((sbox->cam.angle + FOV / 2) / M_PI * 180));
+	ang = sbox->cam.angle + FOV / 2;
+	i = -1;
 	while (++i < WIN_W)
 	{
-		dist = get_dist(sbox, ang, len);
-	dist *= cos(ang - sbox->cam.angle);
-		if (dist > 0.001)
-			h = WALL_H * DIST / dist;
-		else
+		dist = get_dist_x(sbox, ang);
+		ydist = get_dist_y(sbox, ang);
+	//	printf("<%.1f VS %.1f > ", dist, ydist);
+		if (ydist > 0.1 && ydist < dist)
+		{
+			dist = ydist;
+			printf("Y! ");
+		}
+		if (dist < 0.1)
 			h = 0;
+		else
+		{
+			dist *= cos(ang - sbox->cam.angle);
+			h = WALL_H * DIST / dist;
+			printf("X! ");
+		}
 		j = -1;
 		while (++j < WIN_H)
 		{
-			if (j < h)
-				map[i + j * len] = 0x42F4AD;
+			if (j > WIN_H / 2 - h && j < WIN_H / 2 + h)
+				map[i + j * WIN_W] = 0x42F4AD;
 			else
-				map[i + j * len] = 0x0;
+				map[i + j * WIN_W] = 0x0;
 		}
-		ang += STEP;
+		ang -= STEP;
 	}
 }
