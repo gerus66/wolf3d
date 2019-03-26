efbd2f542cc7d7284194acbe7c497e8171e2628c
Merlon crakehall Bartoletti <mbartole@e3r4p10.21-school.ru>
2019-03-26 13:18:12 +0300

    1

--- paint_walls.c
+++ paint_walls.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   paint_walls.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: bturcott <bturcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/25 19:57:27 by mbartole          #+#    #+#             */
-/*   Updated: 2019/03/25 20:08:42 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/03/26 12:53:26 by bturcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,44 @@
 # define W 0xF4D742
 # define S 0xF44262
 
-int		paint_walls(int h, float ang, char fl)
+
+void 	convert_texture(SDL_Texture *texture, SDL_Rect position, SDL_Rect *object, int offset)
+{
+	SDL_Rect curr;
+	SDL_QueryTexture(texture, NULL, NULL, &curr.w, &curr.h);
+	//printf("current == x-> %d y-> %d w-> %d h-> %d\n", curr.x, curr.y, curr.w, curr.h);
+	object->x = offset * curr.w / BLOCK;
+	object->w = 1;
+	object->h = curr.h;
+	object->y = 0;
+	//position->w = curr.w % position->w;
+
+}
+
+
+int select_texture(float ang, char fl)
 {
 	if ((fl == 'x' && QT_12(ang)))
-		return (N);
+		return (0);
 	if ((fl == 'x' && QT_34(ang)))
-		return (S);
+		return (1);
 	if ((fl == 'y' && QT_23(ang)))
-		return (W);
+		return (2);
 	if ((fl == 'y' && QT_14(ang)))
-		return (E);
+		return (3);
+}
+
+int		paint_walls(t_sdl *sdl, float ang, int h, int offset, int x, char side)
+{
+	//printf("%d\n", offset);
+	SDL_Rect texture;
+	SDL_Rect object;
+
+	texture.h = WIN_H / 2 + h;
+	texture.w = 1;
+	texture.x = x;
+	texture.y = WIN_H / 2 - h;
+	convert_texture(sdl->texture_pack[select_texture(ang, side)], texture, &object, offset);
+	SDL_RenderCopy(sdl->render, sdl->texture_pack[select_texture(ang, side)], &object, &texture);
 	return (0xFFFFFF);
 }
