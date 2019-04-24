afc18d7024aacf4c0290ff3991a821ac4eeb742b
Berta Turcotte <bturcott@vo-f2.21-school.ru>
2019-04-24 12:02:25 +0300

    collisions

--- wolf.h
+++ wolf.h
@@ -6,7 +6,7 @@
 /*   By: bturcott <bturcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 22:02:59 by mbartole          #+#    #+#             */
-/*   Updated: 2019/04/23 19:14:22 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/04/24 10:56:11 by bturcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -141,4 +141,5 @@ void			paint_walls(t_sdl *sdl, int *params);
 
 int 			init_music(t_sdl *sdl);
 int				sounds_control_panel(Mix_Chunk **samples, int command);
+void 			sounds(t_sdl *sdl, SDL_Event e);
 #endif
